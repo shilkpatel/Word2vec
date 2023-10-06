a5d7da06546993d3e0005502a474b4bfe82c82d9
Shil16 <shilkpatel2004@gmail.com>
2023-10-06 17:10:20 +0100

    working system
    I now have a word2vec system and will add functions which steamline it.

--- word2vec.py
+++ word2vec.py
@@ -38,6 +38,7 @@ class trainer:
         self.learning=learning_rate
         self.vec_model=model
         self.adjust={}
+        self.last_average_error=1
 
         for i in self.vec_model.vector:
             self.adjust[i]=[[0 for j in range(model.number_of_embedding)] for k in range(2)]
@@ -47,18 +48,19 @@ class trainer:
         context=self.vec_model.vector[training_data[1]][1]
         result= word2vec.dot(main,context)
         result= word2vec.sigmoid(result)
-        error=float(training_data[2])-result
+        #error=float(training_data[2])-result
+        error=result-float(training_data[2])
         error_to_vector=(2*error)*result*(1-result)
 
         #adjust the main vector
         for i in range(len(main)):
-            self.adjust[training_data[0]][0][i]+=(error_to_vector*context[i]*self.learning*-1)
-
+            self.adjust[training_data[0]][0][i]+=(error_to_vector*context[i]*(50*self.last_average_error)*-1)##removing minus
         #adjust in the context vector
         for i in range(len(context)):
-            self.adjust[training_data[1]][1][i]=(error_to_vector*main[i]*self.learning*-1)
+            self.adjust[training_data[1]][1][i]+=(error_to_vector*main[i]*(50*self.last_average_error**2)*-1)##removing minus
+        
 
-        return (math.pow(error,2))
+        return (self.adjust,math.pow(error,2))
 
     def epoch(self,dataset,batch_size):
         sub_set=[]
@@ -68,11 +70,15 @@ class trainer:
             sub_set.append(random.choice(dataset))
 
         for i in sub_set:
-            average_error+=self.learn_single_input(i)
+            learn=self.learn_single_input(i)
+            average_error+=learn[1]
+            self.adjust=learn[0]
+            
+        
         
         for i in self.adjust:
             for j in range(len(self.adjust[i])):
-                for k in range(len(self.adjust[i][j])):
+                for k in range(len(self.adjust[i][j])):         
                     self.adjust[i][j][k]/=batch_size
 
         for i in self.vec_model.vector:
@@ -81,8 +87,10 @@ class trainer:
                     self.vec_model.vector[i][j][k]+=self.adjust[i][j][k]
                     self.adjust[i][j][k]=0
                         
+        self.last_average_error=average_error/batch_size
 
-        print("average_error", average_error/batch_size)
+        print("average_error:", average_error/batch_size)
+        print("learning rate:", self.last_average_error*50)
 
 
 
